29c1936bbde1bb51923b85b54b996093c199fe67
pokerbearkr <ogdongwon@gmail.com>
2025-04-22 12:21:17 +0900

    Initial commit

---
+++ src/main/java/org/example/baedalteam27/BaedalTeam27Application.java
@@ -0,0 +1,13 @@
+package org.example.baedalteam27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalTeam27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaedalTeam27Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/baedalteam27/BaedalTeam27ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.baedalteam27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalTeam27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
