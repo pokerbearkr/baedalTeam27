7ac8e18a46087ff79074856f0b250435a9aeb089
pokerbearkr <ogdongwon@gmail.com>
2025-04-24 16:50:13 +0900

    refactor : final로 변경

--- src/main/java/org/example/baedalteam27/domain/auth/dto/WithdrawRequestDto.java
+++ src/main/java/org/example/baedalteam27/domain/auth/dto/WithdrawRequestDto.java
@@ -1,8 +1,10 @@
 package org.example.baedalteam27.domain.auth.dto;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class WithdrawRequestDto {
-	private String password;
+	private final String password;
 }
