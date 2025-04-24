1915768159580669ed33c1481599673241362758
chohyeokjun <chj3731@naver.com>
2025-04-24 10:34:52 +0900

    refactor: 메서드명 수정, 전체 가게명 페이지 조회 코드 추가, 가계 폐업 조건 추가

--- src/main/java/org/example/baedalteam27/domain/store/service/StoreService.java
+++ src/main/java/org/example/baedalteam27/domain/store/service/StoreService.java
@@ -1,5 +1,6 @@
 package org.example.baedalteam27.domain.store.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.baedalteam27.domain.category.entity.Category;
 import org.example.baedalteam27.domain.category.repository.CategoryRepository;
@@ -32,22 +33,22 @@ public class StoreService {
 
 
     // 가게 등록
-    public SaveStoreResponseDto saveStore(User loginUser, SaveStoreRequestDto requestDto, Long categoryId) {
+    public SaveStoreResponseDto saveStore(Long userId, SaveStoreRequestDto requestDto, Long categoryId) {
         // 유저, 카테고리 조회
-        User user = userRepository.findById(loginUser.getId())
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
 
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new IllegalArgumentException("카테고리가 존재하지 않습니다."));
 
         // 유저가 사장님 권한을 가졌는지 검증
-        if (!loginUser.getRole().equals(UserRole.OWNER)) {
+        if (!user.getRole().equals(UserRole.OWNER)) {
             throw new IllegalArgumentException("가게를 등록할 권한이 없습니다.");
         }
 
         // 소요한 가게가 3개 이하인지 검증
-        int storeCount = storeRepository.countByUserId(loginUser.getId());
-        if (storeCount >= 4) {
+        int storeCount = storeRepository.countByUserIdAndIsDeletedFalse(userId);
+        if (storeCount >= 3) {
             throw new IllegalArgumentException("가게는 최대 3개까지만 등록할 수 있습니다.");
         }
 
@@ -78,9 +79,9 @@ public class StoreService {
 
     // 가게 전체 리스트 조회 (카테고리에 해당하는 가게명 또는 입력받은 단어가 들어가는 가게명)
     public Page<StoreNameResponseDto> findStores(Long categoryId, String storeName, Pageable pageable) {
-        // id 와 name 둘 다 기입하거나 둘 다 기입하지 않았을 때 예외처리
-        if ((categoryId != null && storeName != null) || (categoryId == null && storeName == null)) {
-            throw new IllegalArgumentException("카테고리 또는 가게명 중 하나만 입력!");
+        // 둘 다 입력 했을 때 예외처리
+        if (categoryId != null && storeName != null) {
+            throw new IllegalArgumentException("카테고리 또는 가게명 중 하나만 입력! 또는 둘 다 입력X");
         }
 
         // 입력받은 단어가 들어가는 가게명
@@ -90,27 +91,27 @@ public class StoreService {
         }
 
         // 카테고리에 해당하는 가게명
-        return storeRepository.findByCategoryIdAndIsDeletedFalse(categoryId, pageable)
+        if (categoryId != null) {
+            return storeRepository.findByCategoryIdAndIsDeletedFalse(categoryId, pageable)
+                    .map(store -> new StoreNameResponseDto(store.getId(), store.getStoreName()));
+        }
+
+        // 둘 다 입력하지 않았을 때 전체 리스트 조회
+        return storeRepository.findByIsDeletedFalse(pageable)
                 .map(store -> new StoreNameResponseDto(store.getId(), store.getStoreName()));
     }
 
-    // 가게 단건 조회(카테고리의 기게들 중 하나의 가게 조회, 전체 가게들 중 하나의 가게 조회)
-    public StoreResponseDto findStore(Long categoryId, Long storeId) {
+    // 가게 단건 조회
+    public StoreResponseDto findStore(Long storeId) {
         Store store; // 변수 선언
         // 필수값 검증
         if (storeId == null) {
             throw new IllegalArgumentException("가게 번호는 필수!");
         }
 
-        // 카테고리에 해당하는 가게 찾기
-        if (categoryId != null) {
-            store = storeRepository.findByCategoryIdAndIdAndIsDeletedFalse(categoryId, storeId)
-                    .orElseThrow(() -> new IllegalArgumentException("카테고리에 해당된 가게를 찾을 수 없습니다."));
-        } else {
-            // 전체에서 가게 조회
-            store = storeRepository.findByIdAndIsDeletedFalse(storeId)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 이름의 가게를 찾을 수 없습니다."));
-        }
+        // 전체에서 가게 조회
+        store = storeRepository.findByIdAndIsDeletedFalse(storeId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 이름의 가게를 찾을 수 없습니다."));
 
         // 메뉴를 MenuResponseDto에 담고 리스트로 변환 (N+1 문제 해결 예정)
         List<MenuResponseDto> menuResponseDtoList = store.getMenus().stream()
@@ -129,6 +130,7 @@ public class StoreService {
     }
 
     // 가게 수정
+    @Transactional
     public void updateStore(Long userId, UpdateStoreRequestDto requestDto, Long storeId) {
 
         // 가게 찾기
@@ -152,10 +154,12 @@ public class StoreService {
     }
 
     // 가게 폐업
-    public void deleteStore(User loginUser, Long storeId) {
+    public void deleteStore(Long userId, Long storeId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
 
         // 유저가 사장님 권한을 가졌는지 검증
-        if (!loginUser.getRole().equals(UserRole.OWNER)) {
+        if (!user.getRole().equals(UserRole.OWNER)) {
             throw new IllegalArgumentException("가게 사장님이 아닙니다.");
         }
 
@@ -163,8 +167,13 @@ public class StoreService {
                 .orElseThrow(() -> new IllegalArgumentException("가게가 존재하지 않습니다."));
 
         // 유저가 가게를 등록한 유저인지 검증
-        if (!loginUser.getId().equals(store.getUser().getId())) {
+        if (!user.getId().equals(store.getUser().getId())) {
             throw new IllegalArgumentException("해당 가게에 대한 삭제 권한이 없습니다.");
+        }
+
+        // 폐업한 가게를 다시 폐업하려고 할 때
+        if (store.isDeleted()) {
+            throw new IllegalArgumentException("이미 폐업한 가게 입니다.");
         }
 
         storeRepository.delete(store);
