6d99dd459c3b367c70efc95c43b6ac40fb9666ea
chohyeokjun <chj3731@naver.com>
2025-04-24 17:04:12 +0900

    feat: 공통된 findById 메서드 예외처리

--- src/main/java/org/example/baedalteam27/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/baedalteam27/domain/user/repository/UserRepository.java
@@ -8,4 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
 	boolean existsByEmail(String email);
+
+	default User findByIdOrElseThrow (Long userId) {
+		return findById(userId).orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
+	}
 }
