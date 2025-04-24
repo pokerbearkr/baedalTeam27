bc6c1895d15f45055bc54c80a00f447af6b017e3
chohyeokjun <chj3731@naver.com>
2025-04-24 16:59:41 +0900

    feat: 커스텀 exception 생성

---
+++ src/main/java/org/example/baedalteam27/global/exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package org.example.baedalteam27.global.exception;
+
+public class ForbiddenException extends RuntimeException{
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
