ca36e3da20789529785c952ae4588aed00e9873a
chohyeokjun <chj3731@naver.com>
2025-04-25 11:51:03 +0900

    refactor: InternalServerError 반환을 상태 코드에 적합한 상태 반환하도록 변경

--- src/main/java/org/example/baedalteam27/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/baedalteam27/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.baedalteam27.global.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,13 +10,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handlerIllegalArgumentException (IllegalArgumentException e) {
-        ErrorResponse errorResponse = new ErrorResponse(500, e.getMessage());
-        return ResponseEntity.internalServerError().body(errorResponse);
+        ErrorResponse errorResponse = new ErrorResponse(400, e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<ErrorResponse> handlerForbiddenException (ForbiddenException e) {
+    public ResponseEntity<ErrorResponse> handlerForbiddenException(ForbiddenException e) {
         ErrorResponse errorResponse = new ErrorResponse(403, e.getMessage());
-        return ResponseEntity.internalServerError().body(errorResponse);
-        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
+    }
 }
