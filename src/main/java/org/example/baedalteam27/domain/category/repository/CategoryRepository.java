a9b417efe267c1dffadde4c0e022281a6cdc943e
chohyeokjun <chj3731@naver.com>
2025-04-25 11:36:39 +0900

    refactor: @Repository 삭제

--- src/main/java/org/example/baedalteam27/domain/category/repository/CategoryRepository.java
+++ src/main/java/org/example/baedalteam27/domain/category/repository/CategoryRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     default Category findByIdOrElseThrow(Long categoryid) {
         return findById(categoryid).orElseThrow(() -> new IllegalArgumentException("카테고리가 존재 하지 않습니다."));
