191e6a20ad39b11588a3b0129b29a0803c8b6e36
pokerbearkr <ogdongwon@gmail.com>
2025-04-25 12:08:19 +0900

    feat : 정해진 주소 아니면 filter에서 걸러지게 구현

--- src/main/java/org/example/baedalteam27/global/jwt/JwtProvider.java
+++ src/main/java/org/example/baedalteam27/global/jwt/JwtProvider.java
@@ -21,6 +21,7 @@ public class JwtProvider {
 	private String secret;
 
 	private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1시간
+	private final long REFRESH_TIME = 1000 * 60 * 60 * 24 * 7; // 7일
 
 	@PostConstruct
 	public void init() {
@@ -28,7 +29,7 @@ public class JwtProvider {
 		this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 	}
 
-	public String createToken(Long userId, String role) {
+	public String createAccessToken(Long userId, String role) {
 		return Jwts.builder()
 			.setSubject(userId.toString())
 			.claim("role", role)
@@ -37,6 +38,37 @@ public class JwtProvider {
 			.signWith(key)
 			.compact();
 	}
+
+	public String createRefreshToken() {
+		return Jwts.builder()
+			.setIssuedAt(new Date())
+			.setExpiration(new Date(System.currentTimeMillis() + REFRESH_TIME))
+			.signWith(key)
+			.compact();
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			return false;
+		}
+	}
+
+	public Long extractUserIdAllowExpired(String token) {
+		try {
+			return Long.parseLong(Jwts.parserBuilder()
+				.setSigningKey(key)
+				.build()
+				.parseClaimsJws(token.replace("Bearer ", ""))
+				.getBody()
+				.getSubject());
+		} catch (ExpiredJwtException e) {
+			return Long.parseLong(e.getClaims().getSubject()); // 만료된 토큰이라도 claims 접근 가능
+		}
+	}
+
 
 	public Long getUserIdFromToken(String token) {
 		return Long.parseLong(parseClaims(token).getBody().getSubject());
