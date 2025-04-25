2637bd5ddc464604115d7b24d33f5327393fa017
jieun615 <je060105@gmail.com>
2025-04-25 14:39:43 +0900

    feat : 메뉴 수정, 삭제 기능 구현

--- src/main/java/org/example/baedalteam27/domain/menu/service/MenuService.java
+++ src/main/java/org/example/baedalteam27/domain/menu/service/MenuService.java
@@ -1,5 +1,6 @@
 package org.example.baedalteam27.domain.menu.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.baedalteam27.domain.menu.dto.MenuDto;
 import org.example.baedalteam27.domain.menu.entity.Menu;
@@ -28,5 +29,29 @@ public class MenuService {
                 .build();
 
         return menuRepository.save(menu);
+    }
+
+    @Transactional
+    public Menu updateMenu(Long menuId, MenuDto menuDto) {
+        Menu menu = menuRepository.findById(menuId)
+                .orElseThrow(() -> new IllegalArgumentException("메뉴를 찾을 수 없습니다"));
+
+        Store store = storeRepository.findById(menuDto.getId())
+                .orElseThrow(() -> new IllegalArgumentException("가게를 찾을 수 없습니다"));
+
+        menu.setStore(store);
+        menu.setName(menuDto.getName());
+        menu.setPrice(menuDto.getPrice());
+        menu.setDescription(menuDto.getDescription());
+        menu.setSoldout(menuDto.isSoldOut());
+
+        return menu;
+    }
+
+    @Transactional
+    public void deleteMenu(Long menuId) {
+        Menu menu = menuRepository.findById(menuId)
+                .orElseThrow(() -> new IllegalArgumentException("메뉴를 찾을 수 없습니다"));
+        menuRepository.delete(menu);
     }
 }
