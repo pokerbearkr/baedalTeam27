6695c66ac89e6affc3392f7d9fffae3ac60c33a7
chohyeokjun <chj3731@naver.com>
2025-04-27 18:14:35 +0900

    feat: 가게 등록 실패 test 코드 구현(USER 권한인 경우)

--- src/test/java/org/example/baedalteam27/domain/store/service/StoreServiceTest.java
+++ src/test/java/org/example/baedalteam27/domain/store/service/StoreServiceTest.java
@@ -9,6 +9,8 @@ import org.example.baedalteam27.domain.store.repository.StoreRepository;
 import org.example.baedalteam27.domain.user.UserRole;
 import org.example.baedalteam27.domain.user.entitiy.User;
 import org.example.baedalteam27.domain.user.repository.UserRepository;
+import org.example.baedalteam27.global.exception.CustomException;
+import org.example.baedalteam27.global.exception.ErrorCode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,6 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.ArgumentMatchers.any;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -66,6 +70,27 @@ class StoreServiceTest {
         assertThat(saveStoreResponseDto.getClosedTime()).isEqualTo(closedTime);
         assertThat(saveStoreResponseDto.getMinOrderPrice()).isEqualTo(7000L);
         assertThat(saveStoreResponseDto.getCategoryName()).isEqualTo(category.getName());
+    }
+
+    @Test
+    void saveStore_실패_USER인_경우() {
+        // given
+        Long userId = 1L;
+        User user = new User("email", "password", UserRole.USER, "", "");
+        given(userRepository.getUserByUserId(userId)).willReturn(user);
+
+        Long categoryId = 1L;
+        Category category = new Category("한식");
+        given(categoryRepository.findByIdAndIsDeletedFalseOrElseThrow(categoryId)).willReturn(category);
+
+        LocalTime openTime = LocalTime.parse("16:00:00");
+        LocalTime closedTime = LocalTime.parse("22:00:00");
+        SaveStoreRequestDto dto = new SaveStoreRequestDto("김밥집", "한국", "010", openTime, closedTime, 7000L, categoryId);
+
+        // when
+        // then
+        CustomException customException = assertThrows(CustomException.class, () -> storeService.saveStore(userId, dto));
+        assertEquals(ErrorCode.NOT_OWNER, customException.getErrorCode());
     }
 
     @Test
